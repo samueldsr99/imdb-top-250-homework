8dedfe5203c8d5be66ca8ca4954e7ce284dd2f89
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-05-26 20:51:25 +0200

    refactor: use `images.remotePatterns`

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["m.media-amazon.com"],
+    remotePatterns: [{ protocol: "https", hostname: "m.media-amazon.com" }],
   },
 };
 
