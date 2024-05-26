127c15a12a1b4c282729049de616db336c9cff44
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-05-27 00:16:26 +0200

    feat: update home link with imdb logo

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,26 +1,15 @@
-"use client";
-
-import { usePathname } from "next/navigation";
 import { Link } from "next-view-transitions";
 
-import { clsx } from "@/ui/utils";
+import Logo from "../logo";
 
 import * as styles from "./header.css";
 
-const links = [{ id: 1, name: "Home", href: "/" }];
-
 export default function Header() {
-  const pathname = usePathname();
-
-  const isActive = (href: string) => pathname === href;
-
   return (
     <header className={styles.root}>
-      {links.map(({ id, name, href }) => (
-        <Link key={id} href={href} className={clsx(styles.link, isActive(href) && styles.linkActive)}>
-          {name}
-        </Link>
-      ))}
+      <Link href="/" className={styles.link}>
+        <Logo />
+      </Link>
     </header>
   );
 }
