88a819bbd8a513736a068a3a3c87458162b3c6eb
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-05-27 09:51:09 +0200

    feat: add `useLocalStorage`

---
+++ src/app/hooks/useLocalStorage.ts
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+export default function useLocalStorage<T>(key: string, initialValue: T) {
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  const setValue = (value: T) => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [storedValue, setValue] as const;
+}
