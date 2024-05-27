86357569186c2d2881330e8b385ed9d0b500126c
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-05-27 09:51:36 +0200

    feat: add hearth icons

--- src/ui/icons/index.ts
+++ src/ui/icons/index.ts
@@ -1,4 +1,6 @@
 export { default as ExternalLinkIcon } from "./external-link-icon";
+export { default as HearthIcon } from "./hearth-icon";
+export { default as HearthIconFilled } from "./hearth-icon-filled";
 export { default as MagnifyingGlassIcon } from "./magnifying-glass-icon";
 export { default as StarFilledIcon } from "./star-filled-icon";
 export { default as StarIcon } from "./star-icon";
