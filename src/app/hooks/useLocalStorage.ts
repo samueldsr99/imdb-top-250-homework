0500b4685600b14ef86fd797f6abc67caaffdc69
Samuel David Suárez Rodríguez <samueldsr8@gmail.com>
2024-05-27 09:57:56 +0200

    fix: allow `useLocalStorage` to update cb functions

--- src/app/hooks/useLocalStorage.ts
+++ src/app/hooks/useLocalStorage.ts
@@ -1,15 +1,46 @@
 import { useState } from "react";
 
-export default function useLocalStorage<T>(key: string, initialValue: T) {
+const useLocalStorage = <T>(key: string, value: T) => {
   const [storedValue, setStoredValue] = useState<T>(() => {
-    const item = window.localStorage.getItem(key);
-    return item ? JSON.parse(item) : initialValue;
+    try {
+      if (typeof window !== "undefined") {
+        const item = localStorage?.getItem(key);
+
+        return item ? JSON.parse(item) : value;
+      } else {
+        return value;
+      }
+    } catch (error) {
+      return value;
+    }
   });
 
-  const setValue = (value: T) => {
-    setStoredValue(value);
-    window.localStorage.setItem(key, JSON.stringify(value));
+  const setValue = (value: T | ((_prevValue: T) => T)) => {
+    try {
+      if (typeof value === "function") {
+        setStoredValue((prevValue) => {
+          const newValue = (value as (_prevValue: T) => T)(prevValue);
+          if (newValue !== null && newValue !== undefined) {
+            localStorage?.setItem(key, JSON.stringify(newValue));
+          } else {
+            localStorage.removeItem(key);
+          }
+
+          return newValue;
+        });
+      } else {
+        setStoredValue(value);
+        if (value !== null && value !== undefined) {
+          localStorage?.setItem(key, JSON.stringify(value));
+        } else {
+          localStorage?.removeItem(key);
+        }
+      }
+      // eslint-disable-next-line no-empty
+    } catch (error) {}
   };
 
   return [storedValue, setValue] as const;
-}
+};
+
+export default useLocalStorage;
